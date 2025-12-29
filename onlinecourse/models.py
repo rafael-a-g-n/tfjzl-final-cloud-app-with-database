b38cf8cb5f338f4cc88c8728e362e10d52d8c0b4
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-12-29 18:42:11 +0000

    Adds exam models and improves requirements management
    Introduces models for question, choice, and submission to support exam and quiz functionality.
    
    Refactors requirements for broader compatibility by using minimum versions, reducing dependency issues.
    
    Enhances maintainability and lays the groundwork for future assessment features.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,5 +1,7 @@
 import sys
+
 from django.utils.timezone import now
+
 try:
     from django.db import models
 except Exception:
@@ -7,7 +9,6 @@ except Exception:
     sys.exit()
 
 from django.conf import settings
-import uuid
 
 
 # Instructor model
@@ -29,43 +30,38 @@ class Learner(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
-    STUDENT = 'student'
-    DEVELOPER = 'developer'
-    DATA_SCIENTIST = 'data_scientist'
-    DATABASE_ADMIN = 'dba'
+    STUDENT = "student"
+    DEVELOPER = "developer"
+    DATA_SCIENTIST = "data_scientist"
+    DATABASE_ADMIN = "dba"
     OCCUPATION_CHOICES = [
-        (STUDENT, 'Student'),
-        (DEVELOPER, 'Developer'),
-        (DATA_SCIENTIST, 'Data Scientist'),
-        (DATABASE_ADMIN, 'Database Admin')
+        (STUDENT, "Student"),
+        (DEVELOPER, "Developer"),
+        (DATA_SCIENTIST, "Data Scientist"),
+        (DATABASE_ADMIN, "Database Admin"),
     ]
     occupation = models.CharField(
-        null=False,
-        max_length=20,
-        choices=OCCUPATION_CHOICES,
-        default=STUDENT
+        null=False, max_length=20, choices=OCCUPATION_CHOICES, default=STUDENT
     )
     social_link = models.URLField(max_length=200)
 
     def __str__(self):
-        return self.user.username + "," + \
-               self.occupation
+        return self.user.username + "," + self.occupation
 
 
 # Course model
 class Course(models.Model):
-    name = models.CharField(null=False, max_length=30, default='online course')
-    image = models.ImageField(upload_to='course_images/')
+    name = models.CharField(null=False, max_length=30, default="online course")
+    image = models.ImageField(upload_to="course_images/")
     description = models.CharField(max_length=1000)
     pub_date = models.DateField(null=True)
     instructors = models.ManyToManyField(Instructor)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='Enrollment')
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="Enrollment")
     total_enrollment = models.IntegerField(default=0)
     is_enrolled = False
 
     def __str__(self):
-        return "Name: " + self.name + "," + \
-               "Description: " + self.description
+        return "Name: " + self.name + "," + "Description: " + self.description
 
 
 # Lesson model
@@ -80,14 +76,10 @@ class Lesson(models.Model):
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
 # And we could use the enrollment to track information such as exam submissions
 class Enrollment(models.Model):
-    AUDIT = 'audit'
-    HONOR = 'honor'
-    BETA = 'BETA'
-    COURSE_MODES = [
-        (AUDIT, 'Audit'),
-        (HONOR, 'Honor'),
-        (BETA, 'BETA')
-    ]
+    AUDIT = "audit"
+    HONOR = "honor"
+    BETA = "BETA"
+    COURSE_MODES = [(AUDIT, "Audit"), (HONOR, "Honor"), (BETA, "BETA")]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     date_enrolled = models.DateField(default=now)
@@ -98,6 +90,45 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+
+
+# Question model
+class Question(models.Model):
+    # Has a Many-To-One relationship with course
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # Has question text
+    content = models.CharField(max_length=200)
+    # Has a grade point for each question
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.content
+
+    # method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(
+            is_correct=True, id__in=selected_ids
+        ).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+
+# Choice model
+class Choice(models.Model):
+    # Many-To-One relationship with Question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # The choice text
+    content = models.CharField(max_length=200)
+    # Indicates if this choice is the correct one or not
+    is_correct = models.BooleanField(default=False)
+
+
+# Submission model
+class Submission(models.Model):
+    # Many-To-One relationship with Enrollment
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    # Many-To-Many relationship with Choice
+    choices = models.ManyToManyField(Choice)
