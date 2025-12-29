84696da4373ddc3a2e164dbc978b32dbd9acc31d
rafael-a-g-n <ragn.ryzen@gmail.com>
2025-12-29 19:01:27 +0000

    Adds admin support for Question, Choice, and Submission
    Enables admin registration and management of new models, including inline editing for related objects. Improves content administration and supports easier quiz content creation and review within the admin interface.

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Choice, Course, Instructor, Learner, Lesson, Question, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
@@ -10,16 +11,31 @@ class LessonInline(admin.StackedInline):
     extra = 5
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+    list_display = ("name", "pub_date")
+    list_filter = ["pub_date"]
+    search_fields = ["name", "description"]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ["content"]
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ["title"]
 
 
 # <HINT> Register Question and Choice models here
@@ -28,3 +44,6 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
